aeecf65b55bce1d1813d99b9229ae0c322e4d276
观通 <lijin.jlj@alipay.com>
2015-06-06 13:07:30 +0800

    tweak code style

--- src/states.js
+++ src/states.js
@@ -3,24 +3,23 @@
  */
 
 var _ = require("lodash")
-var util = require("util");
-var events = require("events");
+var util = require("util")
+var events = require("events")
 
 var ActionStates =  ["initial", "started", "ended"]
 
 function States( def ){
   var that = this
   that.def = def
-  that.actions = that.def.tenses ? Object.keys(that.def.tenses) :[]
+  that.actions = that.def.tenses ? Object.keys(that.def.tenses) : []
 
-
-  that.actionTenseMap = _.transform( that.def.tenses, function( result,v,k){
+  that.actionTenseMap = _.transform( that.def.tenses, function( result,v,k ){
     v.forEach(function( state,i ){
       result[state] = [k, ActionStates[i]]
     })
   })
 
-  that.naiveStateMap = _.transform( that.def.naive, function( result,v,k){
+  that.naiveStateMap = _.transform( that.def.naive, function( result,v,k ){
     v.forEach(function( state ){
       result[state] = k
     })
@@ -35,12 +34,12 @@ function States( def ){
     })
   )
 
-  events.EventEmitter.call(this);
+  events.EventEmitter.call(this)
 }
 
-util.inherits(States, events.EventEmitter);
+util.inherits(States, events.EventEmitter)
 
-States.prototype.set = function( name, value){
+States.prototype.set = function( name, value ){
   var lastValue = this.states[name]
   this.states[name] = value
 
@@ -48,39 +47,38 @@ States.prototype.set = function( name, value){
   var thisReadable = this.def.tenses[name] ? this.def.tenses[name][ActionStates.indexOf(value)] : value
 
   //console.log( ActionStates.indexOf(lastValue), lastValue)
-  this.emit("change", thisReadable, lastReadable )
-  this.emit( thisReadable, thisReadable, lastReadable )
+  this.emit("change", thisReadable, lastReadable)
+  this.emit(thisReadable, thisReadable, lastReadable)
 }
 
-States.prototype.changeActionState = function(action, stateIndex){
-  if( !this.def.tenses[action] ) throw new Error("there is no state for action "+action)
+States.prototype.changeActionState = function( action, stateIndex ){
+  if( !this.def.tenses[action] ) throw new Error("there is no state for action " + action)
   this.set(action, ActionStates[stateIndex])
 }
 
-States.prototype.start = function(action){
-  this.changeActionState(action,1)
+States.prototype.start = function( action ){
+  this.changeActionState(action, 1)
 }
 
 States.prototype.end = function( action ){
-  this.changeActionState(action,2)
+  this.changeActionState(action, 2)
 }
 
 States.prototype.reset = function( actionOrState ){
   if( this.def.naive[actionOrState] ){
-    this.set(actionOrState,  null)
+    this.set(actionOrState, null)
   }else{
-    this.changeActionState(actionOrState,0)
+    this.changeActionState(actionOrState, 0)
   }
 }
 
-
 States.prototype.activate = function( state ){
-  if( !this.def.naive[state] ) throw new Error("there is no naive state "+ state)
+  if( !this.def.naive[state] ) throw new Error("there is no naive state " + state)
   this.set(state, this.def.naive[state][0])
 }
 
 States.prototype.deactivate = function( state ) {
-  if( !this.def.naive[state] ) throw new Error("there is no naive state "+ state)
+  if( !this.def.naive[state] ) throw new Error("there is no naive state " + state)
   this.set(state, this.def.naive[state][1])
 }
 
@@ -90,15 +88,14 @@ States.prototype.is = function( state ){
   return argv.every(function( state ){
     if( !_.isArray(state) ) state = [state]
 
-    return _.any(state,function(s){
-      if( !that.actionTenseMap[s] && !that.naiveStateMap[s] ) throw new Error("unknown state "+s)
-      if( s=="valid") console.log(that.naiveStateMap["valid"],that.states.valid )
+    return _.any(state, function( s ){
+      if( !that.actionTenseMap[s] && !that.naiveStateMap[s] ) throw new Error("unknown state " + s)
+      if( s === "valid") console.log(that.naiveStateMap["valid"], that.states.valid)
       return that.actionTenseMap[s]
         ? that.states[ that.actionTenseMap[s][0] ] ===  that.actionTenseMap[s][1]
         : that.states[ that.naiveStateMap[s] ] ===  s
     })
   })
 }
-
 
 module.exports = States
