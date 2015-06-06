a6468a8822d745d30b733787ef3fa71db7a66e45
观通 <lijin.jlj@alipay.com>
2015-06-06 20:22:51 +0800

    add test for frames

--- test/spec/frames.js
+++ test/spec/frames.js
@@ -14,14 +14,32 @@ describe("initialize with empty options", function (){
 
 describe("initialize with options", function(){
   var frames = new Frames({frameLimit: 1})
-  frames.set("name", "lily")
+
   it("options.frameLimit should equal to 1", function(){
     assert.equal(frames.options.frameLimit, 1)
   })
+
   it("get and set test", function(){
+    frames.set("name", "lily")
     assert.equal(frames.get("name"), "lily")
+    frames.set("id", "95527")
+    assert.equal(frames.get("id"), "95527")
+    frames.set({"age": 27, "country": "uk"})
+    assert.equal(frames.get("age"), 27)
+    assert.equal(frames.get("country"), "uk")
+
+    frames.set({"features": {lobby: "soccer", education: "doctor"}})
+    assert.equal(frames.get("features.lobby"), "soccer")
+    assert.equal(frames.get("features.education"), "doctor")
   })
-  frames.commit()
-  frames.commit()
-  assert.equal(frames.historyNames.length, 1)
+
+  it("frame has limit", function() {
+    frames.commit()
+    frames.commit()
+    assert.equal(frames.length, 1)
+  })
+
+  
+
+  
 })
