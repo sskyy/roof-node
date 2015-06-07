b297e8f54cf01af048e442fab09f192e813c04b4
加缪 <jiamiu.hzy@alibaba-inc.com>
2015-06-07 18:21:25 +0800

    add mixin

---
+++ mixin/react.js
@@ -0,0 +1,87 @@
+var _ = require("lodash")
+
+
+function getRef( obj, name ){
+  var ns = !_.isArray(name) ? name.split('.') : name,
+    ref = obj,
+    currentName
+
+  while( currentName = ns.shift() ){
+    if(_.isObject(ref) && ref[currentName]){
+      ref = ref[currentName]
+    }else{
+      ref = undefined
+      break;
+    }
+  }
+
+  return ref
+}
+
+function getStateProxy( randomKey ){
+  var proxyState =  {}
+  proxyState[randomKey] = (new Date()).getTime()
+  return proxyState
+}
+
+function Mixin( data, def ){
+  def = _.defaults(def,{
+    attach : "cursors",
+    cursors : {}
+  })
+  var randomKey = (new Date()).getTime()
+  var mixinInstance = {}
+  var updater
+
+
+  function updateComponentFromDataChange(randomKey){
+    this.setState(getStateProxy(randomKey))
+  }
+
+  mixinInstance[def.attach] = _.mapValues( def.cursors, function( name){
+    var dataRef = getRef( data, name )
+    if( !dataRef ){ console.warn("you are requiring an undefined cursor", name, JSON.stringify(data))}
+    return dataRef
+  })
+
+  mixinInstance.getInitialState = function(){
+    return getStateProxy(randomKey)
+  }
+
+  mixinInstance.componentDidMount = function(){
+    var that = this
+    updater = updateComponentFromDataChange.bind(that,randomKey)
+
+    _.forEach( this[def.attach], function( obj ){
+      if( _.isFunction(obj.on) ){
+        obj.on("change",updater)
+      }
+    })
+  }
+
+  mixinInstance.componentWillUnmount = function(){
+    _.forEach( this[def.attach], function( obj ){
+      if( _.isFunction(obj.on) ){
+        obj.off("change",updater)
+      }
+    })
+  }
+
+
+  //utilities
+  mixinInstance[def.attach]._handleFormChange = function( cursor, field, e ){
+    cursor.set(field, e.target.value)
+  }
+
+  return mixinInstance
+}
+
+
+
+module.exports = Mixin
+
+module.exports.util = {
+  handleFormChange : function( data, field, e ){
+    data.set(field, e.target.value)
+  }
+}
